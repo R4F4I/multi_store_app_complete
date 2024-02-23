4940b1a773bd03a5546a6735a08e5aab68d55dc9
R4F4I-homePc <94185789+R4F4I@users.noreply.github.com>
2024-02-23 16:21:10 +0500

    add textformField with variable length also giving some padding to divider aswell

--- lib/main_screens/upload_product.dart
+++ lib/main_screens/upload_product.dart
@@ -15,6 +15,7 @@ class _UploadProductScreenState extends State<UploadProductScreen> {
       body:SafeArea(
         child: SingleChildScrollView(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
                 children: [
@@ -22,13 +23,41 @@ class _UploadProductScreenState extends State<UploadProductScreen> {
                     color: Colors.blueGrey.shade100,
                     height: MediaQuery.of(context).size.width*0.5,
                     width: MediaQuery.of(context).size.width*0.5,
-                    child: const Center(child: Text('you haven\'t picked \n \n any images yet!'),
+                    child: const Center(
+                      child: 
+                        Text('you haven\'t picked \n \n any images yet!',
+                        style: TextStyle(fontSize: 16),
+                        textAlign: TextAlign.center,
+
+                    
+                    ),
                     ),
                     )
                     ],),
-                    const SizedBox(
-                      height: 30,
-                      child: Divider(color: Colors.yellow,thickness: 1.5,))
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        height: 30,
+                        child: Divider(color: Colors.yellow,thickness: 1.5,)),
+                    ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SizedBox(
+                          width: MediaQuery.of(context).size.width*0.4,
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'price',
+                              hintText: 'price of a unit item',
+                              border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(color: Colors.yellow, width: 1),
+                                borderRadius: BorderRadius.circular(10)),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(color: Colors.blueAccent, width: 2),
+                                borderRadius: BorderRadius.circular(10)),
+                              ),),
+                        ),
+                      )
           ],),
         ),
       )
