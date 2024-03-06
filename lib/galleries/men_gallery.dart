36604f0bdb4a569e024f409906ce65365a71457e
R4F4I-homePc <94185789+R4F4I@users.noreply.github.com>
2024-03-06 15:48:18 +0500

    men_gallery.dart work [004...]
    the actual work

--- lib/galleries/men_gallery.dart
+++ lib/galleries/men_gallery.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_tile.dart';
 
 class MenGalleryScreen extends StatefulWidget {
   const MenGalleryScreen({super.key});
@@ -9,7 +11,7 @@ class MenGalleryScreen extends StatefulWidget {
 }
 
 class _MenGalleryScreenState extends State<MenGalleryScreen> {
-  final Stream<QuerySnapshot> _productsStream = FirebaseFirestore.instance.collection('products').snapshots();
+  final Stream<QuerySnapshot> _productsStream = FirebaseFirestore.instance.collection('products').where('maincateg',isEqualTo: 'men').snapshots(); // filter which fields to show up in which area using this
 
   @override
   Widget build(BuildContext context) {
@@ -21,18 +23,82 @@ class _MenGalleryScreenState extends State<MenGalleryScreen> {
         }
 
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Text("Loading");
+          return const Center(child: CircularProgressIndicator());
         }
-
-        return ListView(
-          children: snapshot.data!.docs.map((DocumentSnapshot document) {
-          Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-            return ListTile(
-              leading: Image(image: NetworkImage(data['proimages'][0])),
-              title: Text(data['proname']),
-              subtitle: Text(data['price'].toString()),
-            );
-          }).toList(),
+        if (snapshot.data!.docs.isEmpty){ //when a section, filetered with '.where(,isEqualTo: )' returns an empty output
+          return const Center(child: Text(
+            'This category has \n \n no items yet!',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 26,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Acme',
+              letterSpacing: 1.5,
+              color: Colors.blueGrey,
+            ),
+            ));
+        }
+        return SingleChildScrollView(
+          child: StaggeredGridView.countBuilder(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: snapshot.data!.docs.length,
+            crossAxisCount: 2, 
+            itemBuilder: (context,index){
+              return Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),),
+                  child: Column(
+                    children:[
+                      ClipRRect(
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(15),
+                          topRight: Radius.circular(15)),
+                        child: Container(
+                          constraints: const BoxConstraints(minHeight: 100,maxHeight: 250),
+                          child: Image(image: NetworkImage(snapshot.data!.docs[index]['proimages'][0]),)
+                          ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text(
+                            snapshot.data!.docs[index]['proname'],
+                            maxLines: 2,
+                            overflow: TextOverflow.fade,
+                            style: TextStyle(
+                              color: Colors.grey.shade600,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600
+                             ),
+                            ),
+                            Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                snapshot.data!.docs[index]['price'].toStringAsFixed(2)+('\$'),
+                                 style: const TextStyle(
+                                  color: Colors.red,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600
+                                ),
+                              ),
+                            IconButton(
+                              onPressed: (){}, 
+                              icon: const Icon(Icons.favorite_border_outlined,color: Colors.red,))
+                            ],)]
+                        ),
+                      ),
+                      
+                      ])
+                ),
+              );
+            }, 
+            staggeredTileBuilder: (context)=>const StaggeredTile.fit(1)),
         );
       },
     );
