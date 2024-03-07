a89fb342b693ba5ef7bb68a5834f656f8cfd5809
R4F4I-homePc <94185789+R4F4I@users.noreply.github.com>
2024-03-07 08:44:41 +0500

    completed [004...]
    turned the staggered view styled view of the products into a widget "ProductModel"

--- lib/galleries/men_gallery.dart
+++ lib/galleries/men_gallery.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:multi_store_app/models/product_model.dart';
 import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
 import 'package:staggered_grid_view_flutter/widgets/staggered_tile.dart';
 
@@ -25,7 +26,7 @@ class _MenGalleryScreenState extends State<MenGalleryScreen> {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(child: CircularProgressIndicator());
         }
-        if (snapshot.data!.docs.isEmpty){ //when a section, filetered with '.where(,isEqualTo: )' returns an empty output
+        if (snapshot.data!.docs.isEmpty){ //when a section, filtered with '.where(,isEqualTo: )' returns an empty output
           return const Center(child: Text(
             'This category has \n \n no items yet!',
             textAlign: TextAlign.center,
@@ -45,58 +46,7 @@ class _MenGalleryScreenState extends State<MenGalleryScreen> {
             itemCount: snapshot.data!.docs.length,
             crossAxisCount: 2, 
             itemBuilder: (context,index){
-              return Padding(
-                padding: const EdgeInsets.all(6.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15),),
-                  child: Column(
-                    children:[
-                      ClipRRect(
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(15),
-                          topRight: Radius.circular(15)),
-                        child: Container(
-                          constraints: const BoxConstraints(minHeight: 100,maxHeight: 250),
-                          child: Image(image: NetworkImage(snapshot.data!.docs[index]['proimages'][0]),)
-                          ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            Text(
-                            snapshot.data!.docs[index]['proname'],
-                            maxLines: 2,
-                            overflow: TextOverflow.fade,
-                            style: TextStyle(
-                              color: Colors.grey.shade600,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600
-                             ),
-                            ),
-                            Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                snapshot.data!.docs[index]['price'].toStringAsFixed(2)+('\$'),
-                                 style: const TextStyle(
-                                  color: Colors.red,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w600
-                                ),
-                              ),
-                            IconButton(
-                              onPressed: (){}, 
-                              icon: const Icon(Icons.favorite_border_outlined,color: Colors.red,))
-                            ],)]
-                        ),
-                      ),
-                      
-                      ])
-                ),
-              );
+              return ProductModel(products: snapshot.data!.docs[index]);
             }, 
             staggeredTileBuilder: (context)=>const StaggeredTile.fit(1)),
         );
