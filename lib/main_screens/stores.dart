de7e68842702e7cd8a1a9a9c211ede0585e1f88c
R4F4I-homePc <94185789+R4F4I@users.noreply.github.com>
2024-03-11 15:26:46 +0500

    finished [010 ...]
    added store logos , custom font

--- lib/main_screens/stores.dart
+++ lib/main_screens/stores.dart
@@ -13,23 +13,52 @@ class StoresScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         title: const AppBarTitle(title: 'Stores'),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('suppliers').snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
-          if (snapshot.hasData){
-            return GridView.builder(
-                itemCount: snapshot.data!.docs.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,),
-                itemBuilder: (context,index){
-                      return SizedBox(
-                          height: 120,
-                          width: 120,
-                          child: Image.asset('images/inapp/store.jpg'),
-              );
-            });
-          }
-          return const Center(child: Text('No Stores'),);
-      }),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection('suppliers').snapshots(),
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
+            if (snapshot.hasData){
+              return GridView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    mainAxisExtent: 200,
+                    crossAxisSpacing: 30,
+                    crossAxisCount: 2,),
+                  itemBuilder: (context,index){
+                        return Column(
+                          children: [
+                            Stack(
+                              children: [
+                                SizedBox(
+                                  height: 120,
+                                  width: 120,
+                                  child: Image.asset('images/inapp/store.jpg'),
+                              ),
+                                Positioned(
+                                    bottom: 28,
+                                    left : 10,
+                                    child: SizedBox(
+                                      height: 48,
+                                      width: 100,
+                                      child: Image.network(snapshot.data!.docs[index]['storelogo'],
+                                        fit: BoxFit.cover,),
+                                ))
+                              ],
+                            ),
+                            Text(snapshot.data!.docs[index]['storename'].toLowerCase(),
+                              style: const TextStyle(
+                                fontSize: 24,
+                                fontFamily: "AkayaTelivigala",
+                              ) ,
+                            )
+                          ],
+                        );
+              });
+            }
+            return const Center(child: Text('No Stores'),);
+        }),
+      ),
     );
   }
 }
