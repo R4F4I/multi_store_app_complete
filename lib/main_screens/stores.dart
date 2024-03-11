0b57f15f6a98276b922a878d5a8878d296c185a2
R4F4I-homePc <94185789+R4F4I@users.noreply.github.com>
2024-03-11 15:05:53 +0500

    starting [010 Streaming Stores]
    starting on the store section

--- lib/main_screens/stores.dart
+++ lib/main_screens/stores.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:multi_store_app/widgets/appbar_widgets.dart';
 
@@ -12,6 +13,23 @@ class StoresScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         title: const AppBarTitle(title: 'Stores'),
       ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('suppliers').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
+          if (snapshot.hasData){
+            return GridView.builder(
+                itemCount: snapshot.data!.docs.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,),
+                itemBuilder: (context,index){
+                      return SizedBox(
+                          height: 120,
+                          width: 120,
+                          child: Image.asset('images/inapp/store.jpg'),
+              );
+            });
+          }
+          return const Center(child: Text('No Stores'),);
+      }),
     );
   }
 }
