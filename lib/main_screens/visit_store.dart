f5ff2002481ca85105ae56e6853bdd83b3c84d71
R4F4I-homePc <94185789+R4F4I@users.noreply.github.com>
2024-03-13 00:06:12 +0500

    finished [011 ...]
    added decoration, fetching store image and name
    
    gave padding to staggeredGridView

--- lib/main_screens/visit_store.dart
+++ lib/main_screens/visit_store.dart
@@ -34,52 +34,116 @@ class _VisitStoreState extends State<VisitStore> {
         if (snapshot.hasData && !snapshot.data!.exists) {
           return const Text("Document does not exist");
         }
-
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Material(child: Center(child: CircularProgressIndicator()));
+        }
         if (snapshot.connectionState == ConnectionState.done) {
           Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
           return Scaffold(
+            backgroundColor: Colors.blueGrey.shade100,
             appBar: AppBar(
               toolbarHeight: 100,
               flexibleSpace: Image.asset(
                 'images/inapp/coverimage.jpg',
                 fit: BoxFit.cover,
               ),
-            ),
-            body: StreamBuilder<QuerySnapshot>(
-              stream: productsStream,
-              builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (snapshot.hasError) {
-                  return const Text('Something went wrong');
-                }
-
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-                if (snapshot.data!.docs.isEmpty){ //when a section, filtered with '.where(,isEqualTo: )' returns an empty output
-                  return const Center(child: Text(
-                    'This category has \n \n no items yet!',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 26,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Acme',
-                      letterSpacing: 1.5,
-                      color: Colors.blueGrey,
+              title: Row(
+                children: [
+                  Container(
+                    height: 80,
+                    width: 80,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 4,color: Colors.yellow),
+                      borderRadius: BorderRadius.circular(15),
                     ),
-                  ));
-                }
-                return SingleChildScrollView(
-                  child: StaggeredGridView.countBuilder(
-                      physics: const NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount: snapshot.data!.docs.length,
-                      crossAxisCount: 2,
-                      itemBuilder: (context,index){
-                        return ProductModel(products: snapshot.data!.docs[index]);
-                      },
-                      staggeredTileBuilder: (context)=>const StaggeredTile.fit(1)),
-                );
-              },
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: Image.network(data['storelogo'],fit:BoxFit.cover)),
+                  ),
+                  SizedBox(
+                    height: 77,
+                    width: MediaQuery.of(context).size.width*0.5,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                data['storename'].toUpperCase(),
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.yellow,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          height: 35,
+                          width: MediaQuery.of(context).size.width*0.3,
+                          decoration: BoxDecoration(
+                            color: Colors.yellow,
+                            border: Border.all(
+                                width: 3,
+                                color: Colors.black),
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              'FOLLOW',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],),
+                  )
+                ],),
+            ),
+            body: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: StreamBuilder<QuerySnapshot>(
+                stream: productsStream,
+                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasError) {
+                    return const Text('Something went wrong');
+                  }
+
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  if (snapshot.data!.docs.isEmpty){
+                    return const Center(child: Text(
+                      'This category has \n \n no items yet!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Acme',
+                        letterSpacing: 1.5,
+                        color: Colors.blueGrey,
+                      ),
+                    ));
+                  }
+                  return SingleChildScrollView(
+                    child: StaggeredGridView.countBuilder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: snapshot.data!.docs.length,
+                        crossAxisCount: 2,
+                        itemBuilder: (context,index){
+                          return ProductModel(products: snapshot.data!.docs[index]);
+                        },
+                        staggeredTileBuilder: (context)=>const StaggeredTile.fit(1)),
+                  );
+                },
+              ),
             ),
           );
         }
