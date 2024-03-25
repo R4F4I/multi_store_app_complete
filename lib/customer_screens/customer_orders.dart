9c6e205a2b1ed1214631ccff291622298acdf2ff
R4F4I-homePc <94185789+R4F4I@users.noreply.github.com>
2024-03-25 22:08:01 +0500

    finished [012 Expansion Tile]
    finished [012 Expansion Tile( Customer Order Model )]
    
    moved all code below itemBuilder into a seperate widget which is imported

--- lib/customer_screens/customer_orders.dart
+++ lib/customer_screens/customer_orders.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:multi_store_app/models/cust_order_model.dart';
 import 'package:multi_store_app/widgets/appbar_widgets.dart';
 
 class CustomerOrders extends StatelessWidget {
@@ -43,119 +44,7 @@ class CustomerOrders extends StatelessWidget {
           return ListView.builder(
             itemCount: snapshot.data!.docs.length,
             itemBuilder: (context,index){
-              var order = snapshot.data!.docs[index];
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                          border: Border.all(color: Colors.yellow),
-                          borderRadius: BorderRadius.circular(15)
-                        ),
-                  child: ExpansionTile(
-                          title: Container(
-                            constraints: const BoxConstraints(maxHeight: 80),
-                            width: double.infinity,
-                            child: Row(
-                              children: [
-                            Padding(
-                                padding: const EdgeInsets.only(right: 15),
-                                child: Container(
-                                  constraints:
-                                      const BoxConstraints(maxHeight: 80, maxWidth: 80),
-                                  child: Image.network(order['orderimage']),
-                                ),
-                            ),
-                            Flexible(
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                  Text(
-                                    order['ordername'],
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 2,
-                                    style: TextStyle(
-                                          fontSize: 13,
-                                          fontWeight: FontWeight.w600,
-                                          color: Colors.grey.shade600
-                                          ),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: [
-                                        Text(('\$ ')+(order['orderprice'].toStringAsFixed(2))),
-                                        Text((' x ')+(order['orderqty'].toString()))
-                                      ],
-                                      ),
-                                    )
-                                  ],
-                                )
-                            )
-                          ],
-                        ),
-                      ),
-                      subtitle: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                        const Text('See more...'),
-                        Text(order['deliverystatus'])
-                      ],),
-                      children: [
-                        Container(
-                          //height: 200,
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                            color: order['deliverystatus'] == 'delivered'
-                              ? Colors.blue.withOpacity(0.2)
-                              : Colors.yellow.withOpacity(0.2),
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(('Name: ')+(order['custname']), style: const TextStyle(fontSize: 15),),
-                                Text(('Phone No.: ')+(order['phone']), style: const TextStyle(fontSize: 15),),
-                                Text(('Email Address: ')+(order['email']), style: const TextStyle(fontSize: 15),),
-                                Text(('Address: ')+(order['address']), style: const TextStyle(fontSize: 15),),
-
-                                Row(
-                                  children: [
-                                    const Text( 'Payment Status: ',style: TextStyle(fontSize: 15)),
-                                    Text(order['paymentstatus'], style: const TextStyle(fontSize: 15,color: Colors.purple))
-                                  ]
-                                ),
-                                Row(
-                                  children: [
-                                    const Text('Delivery Status: ', style: TextStyle(fontSize: 15),),
-                                    Text(order['deliverystatus'], style: const TextStyle(fontSize: 15,color: Colors.green),),
-                                  ],
-                                ),
-                                
-                                order['deliverystatus'] == 'shipping'
-                                  ? Text(('Estimated Delivery Date: ')+(order['deliverydate']), style: const TextStyle(fontSize: 15),) 
-                                  : const Text(''),
-                                
-                                order['deliverystatus'] == 'delivered' && order['orderreview'] == false
-                                  ? TextButton(onPressed: (){}, child: const Text('Write Review'))
-                                  : const Text(''),
-
-                                order['deliverystatus'] == 'delivered' && order['orderreview'] == true
-                                  ? const Row(children: [
-                                      Icon(Icons.check, color: Colors.blue),
-                                      Text('Review added',style: TextStyle(color: Colors.blue,fontStyle: FontStyle.italic),)                                   
-                                    ],)
-
-                                  : const Text('')
-                                  ],
-                                ),
-                              ))
-                        ],
-                      ),
-                    ),
-                  );
+              return CustomerOrderModel(order: snapshot.data!.docs[index]);
                 });
           }),
     );
