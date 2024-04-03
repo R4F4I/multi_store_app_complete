798a6ed07ff555799492b18a58c28e21984d5e32
R4F4I-homePc <94185789+R4F4I@users.noreply.github.com>
2024-04-03 21:24:10 +0500

    completed [006 Statics Screen UI]
    extracted the prev. created element into a widget which is repeated 3 times

--- lib/dashboard/supp_statistics.dart
+++ lib/dashboard/supp_statistics.dart
@@ -11,44 +11,64 @@ class StatisticsScreen extends StatelessWidget {
         title: const AppBarTitle(title: 'Statistics',),
         leading: const AppBarBackButton(),
       ),
-      body: Column(children: [
-        Column(children: [
-              Container(
-                height: 60,
-                width: MediaQuery.of(context).size.width*0.55,
-                decoration: const BoxDecoration(
-                    color: Colors.blueGrey,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(25),
-                        topRight: Radius.circular(25)
-                      )
-                  ),
-                child: Center(child: Text('sold out'.toUpperCase(),style: const TextStyle(color: Colors.white,fontSize: 20),)),
-              ),
-              Container(
-                height: 90,
-                width: MediaQuery.of(context).size.width*0.7,
-                decoration: BoxDecoration(
-                    color: Colors.blueGrey.shade100,
-                    borderRadius: const BorderRadius.only(
-                        bottomLeft: Radius.circular(25),
-                        bottomRight: Radius.circular(25)
-                      )
-                  ),
-                child: const Center(
-                    child: Text('22',
-                      style: TextStyle(
-                          color: Colors.pink,
-                          fontSize: 40,
-                          fontFamily: 'Acme',
-                          letterSpacing: 2,
-                          fontWeight: FontWeight.bold
-                          ),
-                )
-              ),
-              )
-        ],)
-      ],),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+          StatisticModel(label: 'sold out',value: '22',),
+          StatisticModel(label: 'item count',value: '12',),
+          StatisticModel(label: 'total balance',value: '1222.0 \$',),
+          SizedBox(height: 20,)
+        ],),
+      ),
     );
+  }
+}
+
+class StatisticModel extends StatelessWidget {
+  final String label;
+  final dynamic value;
+  const StatisticModel({
+    super.key, required this.label, required this.value
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+          Container(
+            height: 60,
+            width: MediaQuery.of(context).size.width*0.55,
+            decoration: const BoxDecoration(
+                color: Colors.blueGrey,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(25),
+                    topRight: Radius.circular(25)
+                  )
+              ),
+            child: Center(child: Text(label.toUpperCase(),style: const TextStyle(color: Colors.white,fontSize: 20),)),
+          ),
+          Container(
+            height: 90,
+            width: MediaQuery.of(context).size.width*0.7,
+            decoration: BoxDecoration(
+                color: Colors.blueGrey.shade100,
+                borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(25),
+                    bottomRight: Radius.circular(25)
+                  )
+              ),
+            child: Center(
+                child: Text(value,
+                  style: const TextStyle(
+                      color: Colors.pink,
+                      fontSize: 40,
+                      fontFamily: 'Acme',
+                      letterSpacing: 2,
+                      fontWeight: FontWeight.bold
+                      ),
+            )
+          ),
+          )
+    ],);
   }
 }
