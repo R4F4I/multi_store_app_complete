c52c4452a51246760151e10e063cc90a361929c6
R4F4I-NewPC <94185789+R4F4I@users.noreply.github.com>
2024-04-09 00:02:38 +0500

    completed [003 Filtration Search Results]
    >cosmetic changes
    >blank screen when empty prompt

--- lib/minor_screens/search.dart
+++ lib/minor_screens/search.dart
@@ -15,15 +15,18 @@ class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade300,
       appBar: AppBar(
         elevation: 0,
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.grey.shade300,
         leading: IconButton(
           icon: const Icon(Icons.arrow_back_ios_new_rounded, color: Colors.black,
           ),
           onPressed: () { Navigator.pop(context); },
         ),
         title: CupertinoSearchTextField(
+          backgroundColor: Colors.white,
+          autofocus: true, // to auto open keyboard
           onChanged: (value) {
             setState(() {
               searchInput = value;
@@ -31,21 +34,43 @@ class _SearchScreenState extends State<SearchScreen> {
           },
         )
       ),
-      body: StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance
-          .collection('products')          
-          .snapshots(), builder: (BuildContext context, AsyncSnapshot<QuerySnapshot>snapshot) {           
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Material(child: Center(child: CircularProgressIndicator()));
-            }
+      body: searchInput =='' 
+      ? Center(
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.7,
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: Column(
+                  children: [
+                    Icon(
+                      Icons.search,
+                      color: Colors.grey,
+                      size: MediaQuery.of(context).size.height * 0.3,
+                    ),
+                    Text(
+                      'Search Anything...',
+                      style: TextStyle(
+                          fontSize: MediaQuery.of(context).size.height * 0.03,
+                          color: Colors.grey),
+                    )
+                  ],
+                ),
+              ),
+            )
+      : StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance
+            .collection('products')          
+            .snapshots(), builder: (BuildContext context, AsyncSnapshot<QuerySnapshot>snapshot) {           
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Material(child: Center(child: CircularProgressIndicator()));
+              }
 
-            final result = snapshot.data!.docs.where((e) => e['proname'].toLowerCase().contains(searchInput.toLowerCase()));
+              final result = snapshot.data!.docs.where((e) => e['proname'].toLowerCase().contains(searchInput.toLowerCase()));
 
-              return ListView(
-              children:
-                  result.map((e) => Text(e['proname'])).toList(), //in snapshot each unique record is passed into 'e' via .map where a Text widget with val. 'e[proname]' is returned, these returned text widgets are again turned into a list to be passed into list view
-            );
-            }),
+                return ListView(
+                children:
+                    result.map((e) => Text(e['proname'])).toList(), //in snapshot each unique record is passed into 'e' via .map where a Text widget with val. 'e[proname]' is returned, these returned text widgets are again turned into a list to be passed into list view
+              );
+              }),
   );
   }
 }
