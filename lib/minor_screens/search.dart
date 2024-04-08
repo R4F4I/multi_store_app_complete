fc697c0b30356359ab7692e33aa2cf311c87cb8c
R4F4I-NewPC <94185789+R4F4I@users.noreply.github.com>
2024-04-08 23:02:20 +0500

    starting [002 Streaming Products in Search Screen]
    >search screen will now stream products and filter according to prompt
    
    >right now it only streams text containing the name of each product

--- lib/minor_screens/search.dart
+++ lib/minor_screens/search.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +17,19 @@ class SearchScreen extends StatelessWidget {
           onPressed: () { Navigator.pop(context); },
         ),
         title: const CupertinoSearchTextField()
-    )
-    );
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+          .collection('products')          
+          .snapshots(), builder: (BuildContext context, AsyncSnapshot<QuerySnapshot>snapshot) {           
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Material(child: Center(child: CircularProgressIndicator()));
+            }
+              return ListView(
+              children:
+                  snapshot.data!.docs.map((e) => Text(e['proname'])).toList(), //in snapshot each unique record is passed into 'e' via .map where a Text widget with val. 'e[proname]' is returned, these returned text widgets are again turned into a list to be passed into list view
+            );
+            }),
+  );
   }
 }
