f5d4125f7e4bd147c2ceb15e8a82e427febafb28
R4F4I-NewPC <94185789+R4F4I@users.noreply.github.com>
2024-08-04 12:20:35 +0500

    completed [003 Skip On Timer]
    added a countdown on the page to allow the onboarding page to persist on the screen for a set time after which it will leave

--- lib/main_screens/onboarding_screen.dart
+++ lib/main_screens/onboarding_screen.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: avoid_print
+
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class OnboardingScreen extends StatefulWidget {
@@ -8,6 +12,45 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
+
+  Timer? countDownTimer;
+  int seconds = 3;
+
+  @override
+  void initState() {
+    startTimer();
+    super.initState();
+  }
+
+ @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void startTimer(){
+    countDownTimer = Timer.periodic(const Duration(seconds: 1), (timer){
+      setState(() {
+        seconds --;
+      });
+
+      if (seconds < 0) {
+        stopTimer();
+        Navigator.pushReplacementNamed(context,'/customer_home');
+      }
+      print('timer.tick: $timer.tick');
+      print(timer.tick);
+      print('seconds: $seconds');
+      
+    });
+  }
+
+  void stopTimer(){
+    countDownTimer!.cancel();
+  }
+
+   
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +66,14 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
               decoration: BoxDecoration(
                 color: Colors.grey.shade600.withOpacity(0.6),
                 borderRadius: BorderRadius.circular(25)),
-              child: MaterialButton(onPressed:(){Navigator.pushReplacementNamed(context, '/customer_home');}, child:  const Text('skip'),
+              child: MaterialButton(
+                onPressed:(){
+                  stopTimer();
+                  Navigator.pushReplacementNamed(context, '/customer_home');
+                }, 
+                child: seconds <1
+                ?  const Text('skip')
+                :  Text('skip | $seconds'),
               )
               ),
           ),
